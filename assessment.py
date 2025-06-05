1b887fea6f0ba4f9aca4e2a4ce203d37cca43358
JadeonMiller <jadeonmiller707@gmail.com>
2025-06-05 14:39:05 +0200

    setup(assessment): Assessment practice.

---
+++ assessment.py
@@ -0,0 +1,38 @@
+def rot13(phrase:str) -> str:
+    return str()
+
+
+def is_prime(number:int) -> bool:
+    return bool()
+
+
+def is_palindrome(phrase:str) -> bool:
+    return bool()
+
+
+def caesar_cipher(phrase:str, shift:int) -> str:
+    return str()
+
+
+def are_anagrams(phrase1:str, phrase2:str) -> bool:
+    return bool()
+
+
+def check_password_strength(password:str) -> bool:
+    return bool()
+
+
+def fibonacci(number:int) -> list:
+    return list()
+
+
+def factorial(number:int) -> int:
+    return int()
+
+
+def matrix_addition(list_a:list, list_b:list) -> list:
+    return list()
+
+
+def pascal_triangle(n:int) -> list:
+    return list()
